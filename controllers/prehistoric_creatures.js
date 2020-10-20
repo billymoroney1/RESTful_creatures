e55ef343c068c13cffccf57475e1040a37dd7cf3
billymoroney <YOUR-EMAIL-ADDRESS>
2020-10-20 16:02:16 -0500

    prehistoric controller works

--- controllers/prehistoric_creatures.js
+++ controllers/prehistoric_creatures.js
@@ -1,4 +1,35 @@
 const express = require('express')
 const router = express.Router()
+const fs = require('fs')
 
 module.exports = router
+
+router.get('/', (req, res)=>{
+    let prehistoricCreatures = fs.readFileSync('./prehistoric_creatures.json')
+    let creatureData = JSON.parse(prehistoricCreatures)
+    res.render('prehistoric_creatures/index', {myCreatures: creatureData})
+})
+
+router.post('/', (req, res)=>{
+    let creatures = fs.readFileSync('./prehistoric_creatures.json')
+    creatures = JSON.parse(creatures)
+
+    creatures.push(req.body)
+
+    fs.writeFileSync('./prehistoric_creatures.json', JSON.stringify(creatures))
+
+    res.redirect('/prehistoric_creatures')
+})
+
+router.get('/new', (req, res)=>{
+    res.render('prehistoric_creatures/new')
+})
+
+router.get('/:idx', (req, res)=>{
+    let creatures = fs.readFileSync('./prehistoric_creatures.json')
+    let creatureData = JSON.parse(creatures)
+
+    let creatureIndex = parseInt(req.params.idx)
+
+    res.render('prehistoric_creatures/show', {myCreatures: creatureData[creatureIndex]})
+})
--- index.js
+++ index.js
@@ -17,53 +17,8 @@ app.get('/', (req, res)=>{
 })
 
 app.use('/dinosaurs', require('./controllers/dinosaurs'))
-// app.use('/prehistoric_creatures', require('./controllers/prehistoric_creatures'))
+app.use('/prehistoric_creatures', require('./controllers/prehistoric_creatures'))
 
-app.get('/prehistoric_creatures', (req, res)=>{
-    let prehistoricCreatures = fs.readFileSync('./prehistoric_creatures.json')
-    let creatureData = JSON.parse(prehistoricCreatures)
-    res.render('prehistoric_creatures/index', {myCreatures: creatureData})
-})
-
-// POST ROUTE
-
-app.post('/prehistoric_creatures', (req, res)=>{
-    let creatures = fs.readFileSync('./prehistoric_creatures.json')
-    creatures = JSON.parse(creatures)
-
-    creatures.push(req.body)
-
-    fs.writeFileSync('./prehistoric_creatures.json', JSON.stringify(creatures))
-
-    res.redirect('/prehistoric_creatures')
-})
-
-//NEW/READ (GET) ROUTE
-
-
-app.get('/prehistoric_creatures/new', (req, res)=>{
-    res.render('prehistoric_creatures/new')
-})
-
-app.get('/prehistoric_creatures/:idx', (req, res)=>{
-    let creatures = fs.readFileSync('./prehistoric_creatures.json')
-    let creatureData = JSON.parse(creatures)
-
-    let creatureIndex = parseInt(req.params.idx)
-
-    res.render('prehistoric_creatures/show', {myCreatures: creatureData[creatureIndex]})
-})
-
-// SHOW/READ (GET) ROUTE
-
-// app.get('/dinosaurs/:idx', (req, res)=>{
-//     let dinosaurs = fs.readFileSync('./dinosaurs.json')
-//     let dinoData = JSON.parse(dinosaurs)
-
-//     let dinoIndex = parseInt(req.params.idx)
-
-//     res.render('dinosaurs/show', {myDinos: dinoData[dinoIndex]})
-// })
 
 
 
